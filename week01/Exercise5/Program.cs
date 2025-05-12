61a99e8528537abfd3b0d8270f3b5f2531af5ca4
TylerPete <tpeterson4@byupathway.edu>
2025-05-12 18:53:02 -0500

    completed all five learning activities for week 1

--- week01/Exercise5/Program.cs
+++ week01/Exercise5/Program.cs
@@ -4,6 +4,40 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise5 Project.");
+        DisplayWelcome();
+        string userName = PromptUserName();
+        int favNumber = PromptUserNumber();
+        int squaredNumber = SquareNumber(favNumber);
+        DisplayResult(userName, squaredNumber);
+    }
+
+    static void DisplayWelcome()
+    {
+        Console.WriteLine("Welcome to the Program!");
+    }
+
+    static string PromptUserName()
+    {
+        Console.Write("Please enter your name: ");
+        string userName = Console.ReadLine();
+        return userName;
+    }
+
+    static int PromptUserNumber()
+    {
+        Console.Write("Please enter your favorite number: ");
+        string userNumber = Console.ReadLine();
+        int favNumber = int.Parse(userNumber);
+        return favNumber;
+    }
+
+    static int SquareNumber(int originalNumber)
+    {
+        return originalNumber * originalNumber;
+    }
+
+    static void DisplayResult(string userName, int squaredNumber)
+    {
+        Console.WriteLine($"{userName}, the square of your number is {squaredNumber}");
     }
 }
