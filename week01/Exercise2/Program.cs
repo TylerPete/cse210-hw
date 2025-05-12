c36c91c986d013a855fa6d96fc24d806db47b807
TylerPete <tpeterson4@byupathway.edu>
2025-05-11 23:15:47 -0500

    completed through Exercise 2 in Week 1 Learning Activities

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -4,6 +4,58 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.Write("What is your grade percentage? ");
+        string gradeString = Console.ReadLine();
+        int gradePercentage = int.Parse(gradeString);
+        string letter = "";
+
+        int gradeRemainder = gradePercentage % 10;
+        string gradeSign = "";
+
+        if (gradeRemainder >= 7)
+        {
+            gradeSign = "+";
+        }
+        else if (gradeRemainder < 3)
+        {
+            gradeSign = "-";
+        }
+
+        if (gradePercentage >= 90)
+        {
+            letter = "A";
+            if (gradePercentage >= 97)
+            {
+                gradeSign = "";
+            }
+        }
+        else if (gradePercentage >= 80)
+        {
+            letter = "B";
+        }
+        else if (gradePercentage >= 70)
+        {
+            letter = "C";
+        }
+        else if (gradePercentage >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+            gradeSign = "";
+        }
+
+        Console.WriteLine($"Letter grade: {letter}{gradeSign}");
+
+        if (gradePercentage >= 70)
+        {
+            Console.WriteLine("Congratulations on passing the course!");
+        }
+        else
+        {
+            Console.WriteLine("I'm sorry, you did not pass the course. Good luck next time!");
+        }
     }
 }
