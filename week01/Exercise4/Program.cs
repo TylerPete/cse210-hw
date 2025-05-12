079749e070b7855d29588db103e9986e47427435
TylerPete <tpeterson4@byupathway.edu>
2025-05-12 16:21:16 -0500

    majority of learning activity 4 completed for week 1

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -1,9 +1,33 @@
 using System;
+using System.Diagnostics.CodeAnalysis;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise4 Project.");
+        List<int> numbers = new List<int>();
+        Console.WriteLine("Enter a series of numbers, type 0 when finished.");
+
+        Console.Write("Enter a number: ");
+        string response = Console.ReadLine();
+        int number = int.Parse(response);
+        
+        do
+        {
+            numbers.Add(number);
+
+            Console.Write("Enter a number: ");
+            response = Console.ReadLine();
+            number = int.Parse(response);
+        } while (number != 0);
+
+        int sum = 0;
+        foreach (int num in numbers)
+        {
+            sum += num;
+        }
+
+        Console.WriteLine($"The sum is: {sum}");
+    
     }
 }
