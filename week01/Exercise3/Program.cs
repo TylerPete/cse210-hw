58acce2bab08e4a9568c9afbab497342cfa92758
TylerPete <tpeterson4@byupathway.edu>
2025-05-12 00:21:31 -0500

    completed learning activity #3 for week 1

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -1,9 +1,43 @@
 using System;
+using System.Net;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        string response;
+
+        do
+        {
+            Random randomGenerator = new Random();
+            int theNumber = randomGenerator.Next(1, 101);
+
+            Console.Write("What is your guess? ");
+            string guess = Console.ReadLine();
+            int guessNumber = int.Parse(guess);
+            int numGuesses = 1;
+
+            while (guessNumber != theNumber)
+            {
+                if (guessNumber < theNumber)
+                {
+                    Console.WriteLine("Higher");
+                }
+                else if (guessNumber > theNumber)
+                {
+                    Console.WriteLine("Lower");
+                }
+
+                Console.Write("What is your guess? ");
+                guess = Console.ReadLine();
+                guessNumber = int.Parse(guess);
+                numGuesses++;
+            }
+
+            Console.WriteLine($"You guessed it! It took you {numGuesses} tries.");
+
+            Console.Write("Do you want to continue? ");
+            response = Console.ReadLine();
+        } while (response == "yes");
     }
 }
