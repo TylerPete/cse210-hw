37efafbc75feddd1b322202efe0d5451502c92c2
TylerPete <tpeterson4@byupathway.edu>
2025-05-15 15:03:39 -0500

    first commit after beginning work on Journal program

--- week02/Journal/Program.cs
+++ week02/Journal/Program.cs
@@ -1,3 +1,9 @@
+// Purpose: Demonstrate abstraction in practice by writing a program to help
+//      the user record the events of their day by supplying prompts and storing
+//      their responses along with the question and a date.
+// Author: Tyler Peterson
+// Exceeding requirements: TBD
+
 using System;
 
 class Program
