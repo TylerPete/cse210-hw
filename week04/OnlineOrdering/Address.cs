45dff966cc281db41cc7d1213d94e50cd01d3861
TylerPete <tpeterson4@byupathway.edu>
2025-06-03 08:50:02 -0500

    biolife progress today

---
+++ week04/OnlineOrdering/Address.cs
@@ -0,0 +1,28 @@
+using System.Runtime.CompilerServices;
+
+public class Address
+{
+    private string _streetAddress;
+    private string _city;
+    private string _stateOrProvince;
+    private string _country;
+
+    public bool IsInUSA()
+    {
+        return _country.Contains("USA");
+    }
+
+    public string GetDisplayText()
+    {
+        return $"{_streetAddress}\r\n{_city}, {_stateOrProvince}, {_country}";
+    }
+
+    public Address(string streetAddress, string city, string stateOrProvince, string country)
+    {
+        _streetAddress = streetAddress;
+        _city = city;
+        _stateOrProvince = stateOrProvince;
+        _country = country;
+    }
+    
+}
