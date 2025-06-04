51eb46d2b49c9bc98456305e8698399764eb39b1
TylerPete <tpeterson4@byupathway.edu>
2025-06-04 14:52:33 -0500

    completed learning activity for CSE210 Week 5 (Inheritance)

--- week05/Homework/Program.cs
+++ week05/Homework/Program.cs
@@ -4,6 +4,15 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Homework Project.");
+        Assignment baseAssgn = new Assignment("Samuel Bennett", "Multiplication");
+        Console.WriteLine(baseAssgn.GetSummary());
+
+        MathAssignment mathAssgn = new MathAssignment("Roberto Rodriguez", "Fractions", "7.3", "8-19");
+        Console.WriteLine(mathAssgn.GetSummary());
+        Console.WriteLine(mathAssgn.GetHomeworkList());
+
+        WritingAssignment writeAssgn = new WritingAssignment("Mary Waters", "European History", "The Causes of World War II");
+        Console.WriteLine(writeAssgn.GetSummary());
+        Console.WriteLine(writeAssgn.GetWritingInformation());
     }
 }
