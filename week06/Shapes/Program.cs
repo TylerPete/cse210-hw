5f2f3138bdd1d100fc2f88f394fa4aa138d03be2
TylerPete <tpeterson4@byupathway.edu>
2025-06-10 18:07:37 -0500

    completed Polymorphism learning activity for CSE210

--- week06/Shapes/Program.cs
+++ week06/Shapes/Program.cs
@@ -4,6 +4,19 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Shapes Project.");
+        Square square = new Square("red", 15);
+        Rectangle rectangle = new Rectangle(6, 7, "blue");
+        Circle circle = new Circle(8, "green");
+
+        List<Shape> shapeList = new List<Shape>();
+        shapeList.Add(square);
+        shapeList.Add(rectangle);
+        shapeList.Add(circle);
+
+        foreach (Shape shape in shapeList)
+        {
+            Console.WriteLine($"Color: {shape.GetColor()}");
+            Console.WriteLine($"Area: {shape.GetArea()}");
+        }
     }
 }
