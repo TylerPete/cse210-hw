f1eb9a3495491aec06c4aa4450191deedecc4884
TylerPete <tpeterson4@byupathway.edu>
2025-06-12 08:58:11 -0500

    coded Program.cs for EternalQuest project

--- week06/EternalQuest/Program.cs
+++ week06/EternalQuest/Program.cs
@@ -4,6 +4,8 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the EternalQuest Project.");
+        GoalManager gm = new GoalManager();
+
+        gm.Start();
     }
 }
