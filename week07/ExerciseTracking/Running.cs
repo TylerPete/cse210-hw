d2fdc6f524419c431f9b7cc9b9fef25aa69bb904
TylerPete <tpeterson4@byupathway.edu>
2025-06-17 11:16:39 -0500

    fixed file/folder structure wherein the Activity class was stored (oops)

--- week07/ExerciseTracking/Running.cs
+++ week07/ExerciseTracking/Running.cs
@@ -2,10 +2,15 @@ using System.Diagnostics;
 
 public class Running : Activity
 {
-    private double _distance;
+    private double _distanceMiles;
 
     public Running(string dateString, double lengthMinutes, double distance) : base(dateString, lengthMinutes)
     {
-        _distance = distance;
+        _distanceMiles = distance;
+    }
+
+    public override double GetDistance()
+    {
+        //implement here
     }
 }
